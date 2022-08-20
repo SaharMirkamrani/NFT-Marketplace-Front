b33f1dfbab05b340deda965bf77692b97bb24421
Sahar Mirkamrani <shrmrkmrn@gmail.com>
2022-08-20 12:25:40 +0430

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,25 +4,31 @@ import Header from "../components/Header"
 import Head from "next/head"
 import { NotificationProvider } from "web3uikit"
 
-const APP_ID = process.env.NEXT_PUBLIC_APP_ID
-const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://api.studio.thegraph.com/query/32880/nft-marketplace/0.0.1",
+})
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <div>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-                <NotificationProvider>
-                    <Header />
-                    <Component {...pageProps} />
-                </NotificationProvider>
-            </MoralisProvider>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <ApolloProvider client={client}>
+          <NotificationProvider>
+            <Header />
+            <Component {...pageProps} />
+          </NotificationProvider>
+        </ApolloProvider>
+      </MoralisProvider>
+    </div>
+  )
 }
 
 export default MyApp
